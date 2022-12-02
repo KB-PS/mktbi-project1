20ed3b1ff4573d0755bb89d5502327f90a8e7265
Ondrej Svoboda <ondrej.svoboda@keboola.com>
2022-12-02 11:03:36 +0100

    updating formatters

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,62 +4,33 @@ from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid.shared import JsCode
 from st_aggrid.shared import GridUpdateMode
+from st_aggrid.shared import ColumnsAutoSizeMode
 
 st.set_page_config(page_title="Marketing BI", layout="wide") 
-st.title('🎈 Marketing BI - Project 1')
+st.title('Marketing BI - Project 1')
 
+def format(x):
+    return "₹{:,.1f}".format(x)
+def format_pcnt(x):
+    return "{:.1f} %".format(100 * x)
 
 
 orders_df = pd.read_csv("supermarket_data.csv", parse_dates=["Order Date"])
-
-# container = st.container()
-
-# with container:
-#     st.markdown('----')
-#     sliders = st.columns(4)
-#     #st.markdown('----')
-#     sliders2 = st.columns(4)
-#     st.markdown('----')
-
-# with sliders[0]:
-#     sales_slider = st.slider("price", 0.0, 10000.0, [1000.0, 8000.0])
-
-# with sliders[1]:
-#     discount_slider = st.slider("voucher", 0.0, 10000.0, [1000.0, 8000.0])
-
-# with sliders[2]:
-#     adult_slider = st.slider("adults", 0, 30, [2, 8])
-#     children_slider = st.slider("children", 0, 30, [1, 5])
-
-# with sliders[3]:
-#     insurence_slider = st.slider("insuderence", 0.0, 10000.0, [1000.0, 8000.0])
-    
-# with sliders2[0]:
-#     sales_slider = st.slider("price1", 0.0, 10000.0, [1000.0, 8000.0])
-    
-# with sliders2[1]:
-#     sales_slider = st.slider("price3", 0.0, 10000.0, [1000.0, 8000.0])
-
-# with sliders2[2]:
-#     sales_slider = st.slider("price4", 0.0, 10000.0, [1000.0, 8000.0])
-
-# with sliders2[3]:
-#     sales_slider = st.slider("pricej", 0.0, 10000.0, [1000.0, 8000.0])
-    
-# add this
-
+orders_df["Sales Rup"] = orders_df["Sales"]
+orders_df["Sales Rup"] = orders_df["Sales Rup"].apply(format)
+orders_df["Discount"] = orders_df["Discount"].apply(format_pcnt)
 cellsytle_jscode = JsCode(
     """
 function(params) {
     if (params.value.includes('Bakery')) {
         return {
             'color': 'white',
-            'backgroundColor': 'green'
+            'backgroundColor': '#b1cbbb' //light green
         }
     } else if (params.value.includes('Food Grains')){
             return {
                 'color' : 'white',
-                'backgroundColor': 'red'
+                'backgroundColor': '#eea29a'  // light red
                 }
     } else {
         return {
@@ -73,7 +44,7 @@ function(params) {
 
 gb = GridOptionsBuilder.from_dataframe(orders_df)
 gb.configure_pagination(paginationPageSize=25, paginationAutoPageSize=False)
-gb.configure_column("Category", cellStyle=cellsytle_jscode,)
+gb.configure_column("Category", cellStyle=cellsytle_jscode)
 
 gridOptions = gb.build()
 
@@ -82,7 +53,10 @@ data = AgGrid(orders_df,
        allow_unsafe_jscode=True,
        #update_mode=GridUpdateMode.MODEL_CHANGED
        update_mode=GridUpdateMode.FILTERING_CHANGED,
-       enable_enterprise_modules=False
+       enable_enterprise_modules=False,
+       columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS
+       
+            
 )
 
 
